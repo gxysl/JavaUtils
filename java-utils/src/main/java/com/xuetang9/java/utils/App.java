c8008da6d8084cdf402b6ffde6ee3c015baab600
GX_YSL <GX_YSL@LAPTOP-P1MLN3P2>
2017-05-08 16:24:24 +0800

    测试项目

---
+++ java-utils/src/main/java/com/xuetang9/java/utils/App.java
@@ -0,0 +1,13 @@
+package com.xuetang9.java.utils;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
